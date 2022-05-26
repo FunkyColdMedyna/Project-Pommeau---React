13d71d2aca84f7c31f150263d1efcb4cca666206
Chris Medyna <chris.medyna@gmail.com>
2022-05-26 12:12:02 -0400

    barebones adding pages

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+// import { store } from './store';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
-import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-social/bootstrap-social.css';
 import 'font-awesome/css/font-awesome.css';
-
-
+import './index.css';
 
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
-    <App />
+    <Provider>
+    {/* store ={store} */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
 
 
