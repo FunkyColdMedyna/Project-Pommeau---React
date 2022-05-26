de096a8c4e26c6c393a03b227acd400e5c4cb95d
Chris Medyna <chris.medyna@gmail.com>
2022-05-26 12:21:19 -0400

    rendered header and footer somehow

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-// import { store } from './store';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-social/bootstrap-social.css';
@@ -15,12 +13,9 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider>
-    {/* store ={store} */}
-      <BrowserRouter>
+    <BrowserRouter>
         <App />
-      </BrowserRouter>
-    </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
