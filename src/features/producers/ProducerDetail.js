18d896b36be4e69e3fc7360ee2af78a69ccf55b9
FunkyColdMedyna <102168011+FunkyColdMedyna@users.noreply.github.com>
2022-06-02 12:06:07 -0400

    more producer details

--- src/features/producers/ProducerDetail.js
+++ src/features/producers/ProducerDetail.js
@@ -1,81 +1,80 @@
-import { Card, CardImg, CardText, CardBody, Col, CardTitle, CardImgOverlay, Row, Container} from 'reactstrap';
-import '../../styles.css'
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  Col,
+  CardTitle,
+  CardImgOverlay,
+  Row,
+  Container,
+} from "reactstrap";
+import "../../styles.css";
 
-const ProducerDetail = ( {producer} ) => {
-    const { image, name, description } = producer;
+const ProducerDetail = ({ producer }) => {
+  const { image, name, description } = producer;
 
-    return (
-        <>
-        <Container>
+  return (
+      <Container>
         <Row>
-        <Col md='6'>
-            <h3>{name}</h3>
-                <p>Quick Fact</p>
-                <p>Email</p>
-                <p>Website</p>
-                <p>Map</p>
-                <p>Social</p>
-            </Col>
-            <Col md='6'>
-            <img src={image} className='card-detail'/>
+          <Col className="col-12">
+            <Card>
+                <CardTitle>
+                    <h2>{name}</h2>
+                    <h6>Quick Links</h6>
+                </CardTitle>
+                <CardBody>
+                    <Container>
+                        <Row>
+                            <Col md='6'>
+                                            <a
+                                role="button"
+                                className="btn btn-link"
+                                href="mailto:notreal@notreal.com"
+                                >
+                                <i className="fa fa-envelope-o" /> Email:
+                                </a>
+                                <a role="button" className="btn btn-link" href="tel:+12065551234">
+                                <i className="fa fa-phone" /> 1-206-555-1234
+                                </a>
+                                <br />
+                                <br />
+                                <a
+                                className="btn btn-social-icon btn-facebook"
+                                href="http://www.facebook.com/"
+                                >
+                                <i className="fa fa-facebook" />
+                                </a>{" "}
+                                <a
+                                className="btn btn-social-icon btn-twitter"
+                                href="http://twitter.com/"
+                                >
+                                <i className="fa fa-twitter" />
+                                </a>{" "}
+                                <br />
+                                <br />
+                                <a
+                                className="btn btn-social-icon "
+                                href="http://www.maps.google.com"
+                                >
+                                <i className="fa fa-map-marker" />
+                                </a>{" "}
+                                <br />
+                                <br />
+                            </Col>
+                            <Col>
+                                <CardImg src={image} className="card-detail" />
+                            </Col>
+                        </Row>
+                    </Container>
+                    <Container>
+                        <p>{description}</p>
+                    </Container>
+                </CardBody>
+            </Card>
             </Col>
         </Row>
-        <Row>
-            <Col md='12'>
-            <p>{description}</p>
-            </Col>
-        </Row>
-        <Row>
-            <Col md='6'>
-            </Col>
-
-            <Col md='6'>
-            <h5>Producer Social</h5>
-                        <a
-                            className='btn btn-social-icon btn-instagram'
-                            href='http://instagram.com/'
-                        >
-                            <i className='fa fa-instagram' />
-                        </a>{' '}
-                        <a
-                            className='btn btn-social-icon btn-facebook'
-                            href='http://www.facebook.com/'
-                        >
-                            <i className='fa fa-facebook' />
-                        </a>{' '}
-                        <a
-                            className='btn btn-social-icon btn-twitter'
-                            href='http://twitter.com/'
-                        >
-                            <i className='fa fa-twitter' />
-                        </a>{' '}
-                        <a
-                            className='btn btn-social-icon btn-google'
-                            href='http://youtube.com/'
-                        >
-                            <i className='fa fa-youtube' />
-                        </a>
-            </Col>
-        </Row>
-        </Container>
-      
-        </>
-        // <Col md='12' className='m-4'>
-        //     <Card>
-        //         <CardImg 
-        //         className='card-detail'
-        //         top src={image} 
-        //         alt={name} />
-        //         <CardBody>
-        //             <CardText>{description}</CardText>
-        //             {/* add links to external producer sites . google maps. etc. producer contact info */}
-        //         </CardBody>
-        //         <CardImgOverlay>
-        //             <CardTitle>{name}</CardTitle>
-        //         </CardImgOverlay>
-        //     </Card>
-
-        // </Col>
+    </Container> 
     );
 };
 
